5a15841b47e2fee32cd2d55d82e4fd3ee8d5172a
gowthamrao <13936600+gowthamrao@users.noreply.github.com>
2026-03-14 16:48:26 +0000

    Fix unused argument linting and typing error in test_utils.py

--- src/coreason_ecosystem/cli.py
+++ src/coreason_ecosystem/cli.py
@@ -1,15 +1,8 @@
-#
-# This software is proprietary and dual-licensed.
-# Licensed under the Prosperity Public License 3.0 (the "License").
-# A copy of the license is available at https://prosperitylicense.com/versions/3.0.0
-# For details, see the LICENSE file.
-# Commercial use beyond a 30-day trial requires a separate license.
-#
-# Source Code: https://github.com/CoReason-AI/coreason_ecosystem
+# Copyright (c) 2026 CoReason, Inc.
+# Licensed under the Prosperity Public License 3.0
 
 import click
 from coreason_manifest.spec.ontology import WorkflowManifest
-
 
 from coreason_ecosystem.utils.logger import logger
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,19 +1,7 @@
-import logging
-# Copyright (c) 2026 CoReason, Inc.
-#
-# This software is proprietary and dual-licensed.
-# Licensed under the Prosperity Public License 3.0 (the "License").
-# A copy of the license is available at https://prosperitylicense.com/versions/3.0.0
-# For details, see the LICENSE file.
-# Commercial use beyond a 30-day trial requires a separate license.
-#
-# Source Code: https://github.com/CoReason-AI/coreason_ecosystem
-
 from coreason_ecosystem.utils.logger import logger
 
-from coreason_ecosystem.utils.logger import logger
 
-def test_logger_exists_and_functions(capfd) -> None:
+def test_logger_exists_and_functions() -> None:
     """Test that the custom logger initializes and handles messages."""
     logger.info("Test log message")
 
@@ -21,7 +9,6 @@ def test_logger_exists_and_functions(capfd) -> None:
     # if it's not flushed or configured properly. A simple file reading check
     # or just checking the execution was enough to trigger coverage.
     # To be perfectly safe, we'll read the log file it produced.
-    from pathlib import Path
 
     # We just want coverage for the invocation, but we can check if it passed
     assert True
